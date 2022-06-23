08f55451ff2e3c686996c2890eab224ea5391d89
Voxuanan <voxuanan0307@gmail.com>
2022-06-23 15:40:19 +0700

    Get x question & shuffle question

--- controllers/questionCtrl.js
+++ controllers/questionCtrl.js
@@ -1,16 +1,58 @@
 const Exams = require("../models/examModel");
 const Questions = require("../models/questionModel");
 
+function shuffle(array) {
+    let currentIndex = array.length,
+        randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
 const questionCtrl = {
     getQuesionsByExamId: async (req, res) => {
-        //TODO: lấy ra x câu hỏi và shuffle nếu được
         try {
             const { id } = req.params;
+            let { isShuffle = false, questionCount = 0 } = req.query;
             const exam = await Exams.findOne({ _id: id }).populate(
                 "listOfQuestion",
                 "-correctAnswer"
             );
-            res.json({ msg: "Get question by exam id success!", exam });
+            if (questionCount > exam.listOfQuestion.length)
+                questionCount = exam.listOfQuestion.length;
+            if (questionCount < 0) questionCount = 0;
+
+            if (isShuffle && questionCount != 0) {
+                return res.json({
+                    msg: "Get question by exam id success!",
+                    exam: {
+                        ...exam._doc,
+                        listOfQuestion: [...shuffle(exam.listOfQuestion)].slice(0, questionCount),
+                    },
+                });
+            } else if (isShuffle && questionCount == 0) {
+                return res.json({
+                    msg: "Get question by exam id success!",
+                    exam: { ...exam._doc, listOfQuestion: [...shuffle(exam.listOfQuestion)] },
+                });
+            } else if (questionCount != 0) {
+                return res.json({
+                    msg: "Get question by exam id success!",
+                    exam: {
+                        ...exam._doc,
+                        listOfQuestion: exam.listOfQuestion.slice(0, questionCount),
+                    },
+                });
+            } else res.json({ msg: "Get question by exam id success!", exam });
         } catch (error) {
             return res.status(500).json({ msg: error.message });
         }
